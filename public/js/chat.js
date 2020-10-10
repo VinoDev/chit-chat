590029a2f52ed9fb244617296e7a9d9e408195e5
oleg <vinodevmail@gmail.com>
2020-10-10 13:35:34 +0300

    location sharing added

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,15 +2,28 @@ const socket = io()
 
 const msgForm = document.querySelector('.msg-form');
 const msgInput = document.querySelector('.msg-input');
+const shareLocationBtn = document.querySelector('.share-location-btn');
+
+socket.on("message", (message) => {
+    console.log(message);
+})
 
 msgForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    socket.emit("msgFromClient", msgInput.value);
+    socket.emit("message", msgInput.value);
 
     msgInput.value = "";
 })
 
-socket.on("message", (message) => {
-    console.log(message);
+shareLocationBtn.addEventListener("click", () => {
+    if (!navigator.geolocation)
+        return alert("Geolocation is not supported by your browser")
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit("sentLocation", {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -15,10 +15,18 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
     console.log("New WebSocket connection");
     socket.emit("message", "Welcome to the chat app, this is a test message!");
+    socket.broadcast.emit("message", "A new user has joined the chat!");
 
-    socket.on("msgFromClient", (message) => {
-        console.log(message);
+    socket.on("message", (message) => {
         io.emit("message", message);
+    })
+
+    socket.on("sentLocation", (coords) => {
+        io.emit("message", `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+    })
+
+    socket.on("disconnect", () => {
+        io.emit("message", "A user has left the chat!");
     })
 })
 
