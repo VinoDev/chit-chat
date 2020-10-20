86676b8f6a962a9dcf655f8f9f89f34731b93717
oleg <vinodevmail@gmail.com>
2020-10-20 12:48:57 +0300

    added basic user tracking

--- public/js/chat.js
+++ public/js/chat.js
@@ -66,4 +66,9 @@ shareLocationButton.addEventListener("click", () => {
     })
 })
 
-socket.emit('join', {username, room})
+socket.emit('join', {username, room}, (error) => {
+    if(error){
+        alert(error);
+        location.href = '/';
+    }
+})
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
 const { generateMessage, generateLocationMessage } = require('./utils/messages');
+const { addUser, removeUser, getUser, getUsersRoom } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -17,10 +18,17 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
     console.log("New WebSocket connection");
 
-    socket.on("join", ({username, room}) => {
-        socket.join(room)
+    socket.on("join", ({username, room}, callback) => {
+
+        const { user, error } = addUser({id: socket.id, username, room})
+
+        if(error) {
+            return callback(error);
+        }
+
+        socket.join(user.room)
         socket.emit("message", generateMessage("Welcome!"));
-        socket.broadcast.to(room).emit("message", generateMessage(`A ${username} has joined the chat!`));
+        socket.broadcast.to(user.room).emit("message", generateMessage(`${user.username} has joined!`));
     })
 
     socket.on("sendMessage", (message, callback) => {
@@ -39,7 +47,11 @@ io.on('connection', (socket) => {
     })
 
     socket.on("disconnect", () => {
-        io.emit("message", generateMessage("A user has left the chat!"));
+        const user = removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit("message", generateMessage(`${user.username} has left!`));
+        }  
     })
 })
 
