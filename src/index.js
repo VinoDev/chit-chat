2154963441bbd52c1497866be9ce3b91fa31087a
oleg <vinodevmail@gmail.com>
2020-10-20 13:49:14 +0300

    added name to messages, and messages to rooms

--- src/index.js
+++ src/index.js
@@ -18,40 +18,43 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
     console.log("New WebSocket connection");
 
-    socket.on("join", ({username, room}, callback) => {
+    socket.on("join", ({ username, room }, callback) => {
 
-        const { user, error } = addUser({id: socket.id, username, room})
+        const { user, error } = addUser({ id: socket.id, username, room })
 
-        if(error) {
+        if (error) {
             return callback(error);
         }
 
         socket.join(user.room)
-        socket.emit("message", generateMessage("Welcome!"));
-        socket.broadcast.to(user.room).emit("message", generateMessage(`${user.username} has joined!`));
+        socket.emit("message", generateMessage("admin", "Welcome!"));
+        socket.broadcast.to(user.room).emit("message", generateMessage("admin", `${user.username} has joined!`));
     })
 
     socket.on("sendMessage", (message, callback) => {
+        const user = getUser(socket.id)
+
         const filter = new Filter();
 
         if (filter.isProfane(message))
             return callback("Profanity is not allowed");
 
-        io.emit("message", generateMessage(message));
+        io.to(user.room).emit("message", generateMessage(user.username, message));
         callback()
     })
 
     socket.on("sentLocation", (coords, callback) => {
-        io.emit("locationMessage", generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
+        const user = getUser(socket.id)
+        io.to(user.room).emit("locationMessage", generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
         callback();
     })
 
     socket.on("disconnect", () => {
         const user = removeUser(socket.id);
 
-        if(user){
-            io.to(user.room).emit("message", generateMessage(`${user.username} has left!`));
-        }  
+        if (user) {
+            io.to(user.room).emit("message", generateMessage("admin", `${user.username} has left!`));
+        }
     })
 })
 
